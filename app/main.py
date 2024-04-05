e8d1b2eb49b42c889ac4479e5907f62edb9e56c5
Justin Han <gksksla7140@gmail.com>
2024-04-04 19:16:54 -0700

    Add CORS middleware and create join_game endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, status, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.models import game
 from .game_manager import GameManager
@@ -7,95 +7,55 @@ from .models import Player, JoinRequest, CreateRequest
 from .utils import generate_game_id
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 manager = GameManager()
 
 
-@app.post("/create_game/")
+@app.post("/create_game", status_code=status.HTTP_201_CREATED)
 async def create_game(create_request: CreateRequest):
     game_id = generate_game_id()
-    while game_id in manager.games:
+    while manager.create_game(create_request.host_id, game_id) is False:
         game_id = generate_game_id()
 
-    manager.create_game(create_request.host_id, "TestGame")
-
     return {"message": "Game created", "game_id": game_id}
+
+
+@app.post("/join_game", status_code=status.HTTP_201_CREATED)
+async def join_game(join_request: JoinRequest):
+    game_id = join_request.game_id
+    player_id = join_request.player_id
+
+    try:
+        game = manager.get_game(game_id)
+        game.add_player(Player(player_id))
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+    return {"message": "Player added to game"}
 
 
 @app.websocket("/ws/{game_id}/{player_id}")
 async def websocket_endpoint(websocket: WebSocket, game_id: str, player_id: str):
     await websocket.accept()
 
-    player = Player(player_id=player_id, websocket=websocket)
-
     try:
-        manager.add_player_to_game(game_id, player)
+        game = manager.get_game(game_id)
+        game.add_websocket_to_player(player_id, websocket)
     except ValueError as e:
-        await websocket.send_text(str(e))
-        return
-
-    await manager.broadcast_to_game(game_id)
+        raise HTTPException(status_code=400, detail=str(e))
 
     try:
         while True:
             data = await websocket.receive_text()
-            manager.games[game_id].type_character(player_id, data)
-            await manager.games[game_id].broadcast()
+            await game.type_character(player_id, data)
     except WebSocketDisconnect:
-        websocket.close()
-        manager.remove_player_from_game(game_id, player_id)
-        await manager.games[game_id].broadcast()
-
-
-html = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>TypeRacer Game</title>
-</head>
-<body>
-    <h1>TypeRacer Game</h1>
-    <h2 id="game-name">Game Name</h2>
-    <div>
-        <h3>Text Events:</h3>
-        <ul id="messages"></ul>
-    </div>
-    <div>
-        <h3>JSON Events:</h3>
-        <ul id="json-events"></ul>
-    </div>
-    <form onsubmit="sendMessage(event); return false;">
-        <input type="text" id="messageText" autocomplete="off"/>
-        <button>Send</button>
-    </form>
-    <script>
-        var client_id = Math.floor(Math.random() * 1000000);
-        var ws = new WebSocket(`ws://localhost:8000/ws/TestGame/${client_id}`);
-        ws.onmessage = function(event) {
-            var data = JSON.parse(event.data);
-            if (data.type === "progress") {
-                document.getElementById('progress').innerText = "Your progress: " + data.progress;
-            } else {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(data.message)
-                message.appendChild(content)
-                messages.appendChild(message)
-            }
-        };
-        function sendMessage(event) {
-            var input = document.getElementById("messageText")
-            ws.send(input.value)
-            input.value = ''
-            event.preventDefault()
-        }
-    </script>
-</body>
-</html>
-
-
-"""
-
-
-@app.get("/")
-async def get():
-    return HTMLResponse(content=html, status_code=200)
+        game.remove_player(player_id)
+        await manager.games[game_id].broadcast_progress()
